7c7fa22cb8a9fd7bf9169b53b39dd52eabf66789
gustavogomez2005 <gustavogomez2005@gmail.com>
2026-04-24 13:01:24 -0300

    Creando arrays de objetos

---
+++ js/archivo.js
@@ -0,0 +1,20 @@
+let Calificaciones = [
+    {
+        alumno: "José",
+        materia: "Matemáticas",
+        primeraNota: 5,
+        segundaNota: 7,
+        terceraNota: 7,
+        promedio: 9
+    },
+];
+
+let nom, mat, nota1, nota2, nota3, prom;
+let unAlumno = {
+        alumno: nom,
+        materia: mat,
+        primeraNota: nota1,
+        segundaNota: nota2,
+        terceraNota: nota3,
+        promedio: prom   
+}
